4d94f404af5b8fb104c0eb5c67da5fcbaa7e7bdc
Ruben S. Garcia <rgarci12@mercadona.es>
2023-06-17 21:43:38 +0200

    Initial commit

---
+++ src/App.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react'
+import App from './App'
+
+describe('App tests', () => {
+  it('should have the title visible', () => {
+    render(<App />)
+    expect(screen.getByText(/Vite \+ React/i)).toBeInTheDocument()
+  })
+})
